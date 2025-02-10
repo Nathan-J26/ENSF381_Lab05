9b85cbf8463e76bda9d79a67374eec608e499ab2
Nathan-J26 <nathanjourdain26@gmail.com>
2025-02-10 11:35:53 -0700

    fixed Engine.java and added missing setter

--- edu/ucalgary/oop/Engine.java
+++ edu/ucalgary/oop/Engine.java
@@ -1,7 +1,7 @@
 public class Engine {
     private String engineType;
 
-    public Engine(string engineType) {
+    public Engine(String engineType) {
         this.engineType = engineType;
     }
 
@@ -11,5 +11,9 @@ public class Engine {
 
     public String getEngineType() {
         return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
     }
 }
