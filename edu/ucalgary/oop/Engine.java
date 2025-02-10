76ecc772266d786794ea03d5c2bf7d5cda4cb9a6
Hania Aamir <147211103+haniaaaamir@users.noreply.github.com>
2025-02-10 13:34:07 -0500

    Update Engine.java

--- edu/ucalgary/oop/Engine.java
+++ edu/ucalgary/oop/Engine.java
@@ -1,3 +1,15 @@
 public class Engine {
+    private String engineType;
 
+    public Engine(string engineType) {
+        this.engineType = engineType;
+    }
+
+    public Engine(Engine other) {
+        this.engineType = other.engineType;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
 }
