cebbe362f03c1a4d9a3714c61fe9e7232adf81eb
Nathan-J26 <nathanjourdain26@gmail.com>
2025-02-10 11:31:10 -0700

    added driver.java

---
+++ edu/ucalgary/oop/Driver.java
@@ -0,0 +1,16 @@
+public class Driver {
+    private String name;
+
+    public Driver(String name) {
+        this.name = name;
+    }
+    public Driver(Driver other) {
+        this.name = other.name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setname(String name) {
+        this.name = name;
+    }
+}
